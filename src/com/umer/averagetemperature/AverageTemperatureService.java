ddb3608069d13097249938c048dd0ee813208a09
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-11-25 21:24:37 +0100

    Feature: Created empty classes.

---
+++ src/com/umer/averagetemperature/AverageTemperatureService.java
@@ -0,0 +1,5 @@
+package com.umer.averagetemperature;
+
+public class AverageTemperatureService {
+
+}
---
+++ src/com/umer/main/Execute.java
@@ -0,0 +1,14 @@
+package com.umer.main;
+
+import java.util.Scanner;
+
+public class Execute {
+
+	public static void main(String[] args) {
+		Scanner console=new Scanner(System.in);
+		System.out.print("How may days' temperature should be calculated: ");
+		int numberOfDays=console.nextInt();
+
+	}
+
+}
