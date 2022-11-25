9909e787819fbbb9cd2bd72fcab43584f5fd92da
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-11-25 22:45:14 +0100

    Feature: Implemented the logic for determining the average temperature without using arrays.

--- src/com/umer/averagetemperature/AverageTemperatureService.java
+++ src/com/umer/averagetemperature/AverageTemperatureService.java
@@ -1,5 +1,22 @@
 package com.umer.averagetemperature;
 
+import java.util.Scanner;
+
 public class AverageTemperatureService {
+
+	public void calculateAverageTemperature() {
+		Scanner console = new Scanner(System.in);
+		System.out.print("How may days' temperature should be calculated: ");
+		int numberOfDays = console.nextInt();
+		int sum = 0;
+		for (int i = 0; i <= numberOfDays; i++) {
+			System.out.print("Day "+i+"'s high temperature: ");
+			int next=console.nextInt();
+			sum+=next;
+		}
+		double average =sum/numberOfDays;
+		System.out.println();
+		System.out.println("Average Temp = "+average);
+	}
 
 }
