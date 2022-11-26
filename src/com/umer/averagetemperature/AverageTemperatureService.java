b78c00fafe6a98f8bf861340249e38b86e31626a
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-11-26 13:27:25 +0100

    Feature: Implemented Average temprature measurement service using arrays.

--- src/com/umer/averagetemperature/AverageTemperatureService.java
+++ src/com/umer/averagetemperature/AverageTemperatureService.java
@@ -8,15 +8,29 @@ public class AverageTemperatureService {
 		Scanner console = new Scanner(System.in);
 		System.out.print("How may days' temperature should be calculated: ");
 		int numberOfDays = console.nextInt();
+		int[] temperatures = new int[numberOfDays];
+		// record temperatures and find average
 		int sum = 0;
-		for (int i = 0; i <= numberOfDays; i++) {
-			System.out.print("Day "+i+"'s high temperature: ");
-			int next=console.nextInt();
-			sum+=next;
+		for (int currentDay = 0; currentDay <= numberOfDays - 1; currentDay++) {
+			System.out.print("Day " + (currentDay + 1) + "'s high temperature: ");
+			temperatures[currentDay] = console.nextInt();
+			sum += temperatures[currentDay];
 		}
-		double average =sum/numberOfDays;
+		double average = sum / numberOfDays;
+
+		// count number of days which are above average
+		int aboveAverageTemperatureDays = 0;
+		for (int currentDay = 0; currentDay <= temperatures.length - 1; currentDay++) {
+			if (temperatures[currentDay] > Math.ceil(average)) {
+				// current day's temperature is above average
+				aboveAverageTemperatureDays++;
+			}
+		}
 		System.out.println();
-		System.out.println("Average Temp = "+average);
+		System.out.println("Average Temp = " + average);
+		System.out.println();
+		System.out.println("number of days on which the temparature is above average is " + aboveAverageTemperatureDays
+				+ " days.");
 	}
 
 }
--- src/com/umer/main/Execute.java
+++ src/com/umer/main/Execute.java
@@ -1,8 +1,14 @@
 package com.umer.main;
 
+import com.umer.averagetemperature.AverageTemperatureService;
+
 public class Execute {
 
 	public static void main(String[] args) {
+		System.out.println("Calling Average temperature calculation service");
+		AverageTemperatureService averageTemperatureService=new AverageTemperatureService();
+		averageTemperatureService.calculateAverageTemperature();
+		System.out.println();
 		
 
 	}
