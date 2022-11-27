a59702e801f57bf88ca5194e76854bf06cda60ae
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-11-27 15:25:37 +0100

    Feature: Implemented the logic for finding the missing number in a number series from 1,2,3,...,(n-2),(n-1),n.

--- src/com/umer/main/Execute.java
+++ src/com/umer/main/Execute.java
@@ -1,14 +1,19 @@
 package com.umer.main;
 
 import com.umer.averagetemperature.AverageTemperatureService;
+import com.umer.missingnumber.MissingNumberService;
 
 public class Execute {
 
 	public static void main(String[] args) {
 		System.out.println("Calling Average temperature calculation service");
 		AverageTemperatureService averageTemperatureService=new AverageTemperatureService();
-		averageTemperatureService.calculateAverageTemperature();
+//		averageTemperatureService.calculateAverageTemperature();
 		System.out.println();
+		
+		MissingNumberService missingNumberService=new MissingNumberService();
+		int[] intArray= {1,2,3,4,5,6,7,8,9,10,11,12,13,15};
+		missingNumberService.findMissingNumber(intArray);
 		
 
 	}
--- src/com/umer/missingnumber/MissingNumberService.java
+++ src/com/umer/missingnumber/MissingNumberService.java
@@ -1,7 +1,25 @@
 package com.umer.missingnumber;
 
 public class MissingNumberService {
-	
-	
+
+	public void findMissingNumber(int[] intArray) {
+		int sum1 = 0;
+		int sum2 = 0;
+		for (int i : intArray) {
+			sum1 += i;
+		}
+		int numberOfElements = intArray.length + 1;
+		/*
+		 * 
+		 * Mathematical formula: Sum of number series from 1,2,3,4,..., (n-2),(n-1),n
+		 * =>n*(n+1)/2
+		 * 
+		 */
+
+		sum2 = numberOfElements * (numberOfElements + 1) / 2;
+		int difference = sum2 - sum1;
+		System.out.println("Missing number is " + difference);
+
+	}
 
 }
