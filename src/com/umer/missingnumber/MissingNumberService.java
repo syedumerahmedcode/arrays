6ca5cc40896f1cf19f21a6ff75e8195375fe9123
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-11-27 15:28:44 +0100

    Feature: Code formatting.

--- src/com/umer/missingnumber/MissingNumberService.java
+++ src/com/umer/missingnumber/MissingNumberService.java
@@ -3,12 +3,13 @@ package com.umer.missingnumber;
 public class MissingNumberService {
 
 	public void findMissingNumber(int[] intArray) {
-		int sum1 = 0;
-		int sum2 = 0;
+		int sumFromArrayElements = 0;
+		int sumUsingMathematicalNumberSeriesFormula = 0;
 		for (int i : intArray) {
-			sum1 += i;
+			sumFromArrayElements += i;
 		}
 		int numberOfElements = intArray.length + 1;
+		
 		/*
 		 * 
 		 * Mathematical formula: Sum of number series from 1,2,3,4,..., (n-2),(n-1),n
@@ -16,8 +17,8 @@ public class MissingNumberService {
 		 * 
 		 */
 
-		sum2 = numberOfElements * (numberOfElements + 1) / 2;
-		int difference = sum2 - sum1;
+		sumUsingMathematicalNumberSeriesFormula = numberOfElements * (numberOfElements + 1) / 2;
+		int difference = sumUsingMathematicalNumberSeriesFormula - sumFromArrayElements;
 		System.out.println("Missing number is " + difference);
 
 	}
