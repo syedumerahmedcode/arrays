28d7e698ad21026f503befcb800c207b13e0fee8
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2023-01-16 19:54:34 +0100

    Feature: Created an empty class structure for finding pairs of numbers whose sum is equal to the given number.

---
+++ src/com/umer/findpair/FindingPairService.java
@@ -0,0 +1,26 @@
+package com.umer.findpair;
+
+public class FindingPairService {
+
+	/**
+	 * Write a program to find all pairs of integers whose sum is equal to a given
+	 * number.
+	 * 
+	 * Examples
+	 * 
+	 * Input: nums = [2,7,11,15], target = 9 Output: [0,1] Output: Because nums[0] +
+	 * nums[1] == 9, we return [0, 1]
+	 * 
+	 * 
+	 * Input: nums = [3,2,4], target = 6 Output: [1,2]
+	 * 
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+	public int[] twoSum(int[] nums, int target) {
+
+		return null;
+	}
+
+}
