5a7ddf04402a0abad64186e210990cc69d317400
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2023-01-17 19:41:31 +0100

    Feature: Created an empty class for searching a number in an array.

---
+++ src/com/umer/searchinarray/SearchInArrayService.java
@@ -0,0 +1,18 @@
+package com.umer.searchinarray;
+
+public class SearchInArrayService {
+
+	/**
+	 * Write a program to to check if an array contains a number in Java.
+	 * 
+	 * int[] intArray = {1,2,3,4,5,6}; searchInArray(intArray, 6); // 5
+	 * 
+	 * @param intArray      The input array to look through.
+	 * @param valueToSearch The number to search in the array.
+	 * @return 0 or positive number if the number is found. Otherwise, -1 is
+	 *         returned.
+	 */
+	public int searchInArray(int[] intArray, int valueToSearch) {
+		return -1;
+	}
+}
