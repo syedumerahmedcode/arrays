341b19fd336a66a6cf3e5a2f9097ddc9b65d3b4e
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2023-01-17 19:43:59 +0100

    Feature: Created the logic for searching the number in an array.

--- src/com/umer/searchinarray/SearchInArrayService.java
+++ src/com/umer/searchinarray/SearchInArrayService.java
@@ -13,6 +13,11 @@ public class SearchInArrayService {
 	 *         returned.
 	 */
 	public int searchInArray(int[] intArray, int valueToSearch) {
+		for (int i = 0; i < intArray.length; i++) {
+			if (intArray[i] == valueToSearch) {
+				return i;
+			}
+		}
 		return -1;
 	}
 }
