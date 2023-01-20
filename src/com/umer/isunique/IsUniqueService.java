3e42c2fe6e2bed0bb32910caa19bd72d876bbd4c
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2023-01-20 19:23:44 +0100

    Feature: Created an empty class for check if the array is unique.

---
+++ src/com/umer/isunique/IsUniqueService.java
@@ -0,0 +1,18 @@
+package com.umer.isunique;
+
+public class IsUniqueService {
+
+	/**
+	 * Write a program to check if an array is unique or not.
+	 * 
+	 * Example
+	 * 
+	 * int[] intArray = {1,2,3,4,5,6}; isUnique(intArray) // true
+	 * 
+	 * @param intArray
+	 * @return
+	 */
+	public boolean isUnique(int[] intArray) {
+		return false;
+	}
+}
