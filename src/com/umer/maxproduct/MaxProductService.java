bc79edb890a51114385eea52a39c00e856890c91
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2023-01-20 19:04:44 +0100

    Feature: Implemented the logic for finding max product service.

--- src/com/umer/maxproduct/MaxProductService.java
+++ src/com/umer/maxproduct/MaxProductService.java
@@ -2,7 +2,29 @@ package com.umer.maxproduct;
 
 public class MaxProductService {
 
-	public String maxProduct(int[] intArray) throws Exception {
-		throw new Exception("To be implemented");
+	/**
+	 * How to find maximum product of two integers in the array where all elements
+	 * are positive.
+	 * 
+	 * Example
+	 * 
+	 * int[] intArray = {10,20,30,40,50}; maxProduct(intArray) // (40,50)
+	 * 
+	 * @param intArray
+	 * @return
+	 * @throws Exception
+	 */
+	public String maxProduct(int[] intArray) {
+		int maxProduct = 0;
+		String pairs = "";
+		for (int i = 0; i < intArray.length; i++) {
+			for (int j = i + 1; j < intArray.length; j++) {
+				if (intArray[i] * intArray[j] > maxProduct) {
+					maxProduct = intArray[i] * intArray[j];
+					pairs = "(" + intArray[i] + " , " + intArray[j] + ")";
+				}
+			}
+		}
+		return pairs;
 	}
 }
