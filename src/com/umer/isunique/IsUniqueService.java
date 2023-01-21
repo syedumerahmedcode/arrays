2c273d0b79836f42533b0917279c0ca3f09104ba
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2023-01-21 19:40:07 +0100

    Feature: Implemneted the logic for determining if the array is unikque or not.

--- src/com/umer/isunique/IsUniqueService.java
+++ src/com/umer/isunique/IsUniqueService.java
@@ -13,6 +13,13 @@ public class IsUniqueService {
 	 * @return
 	 */
 	public boolean isUnique(int[] intArray) {
-		return false;
+		for (int i = 0; i < intArray.length; i++) {
+			for (int j = i+1; j < intArray.length; j++) {
+				if (intArray[i] == intArray[j]) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 }
