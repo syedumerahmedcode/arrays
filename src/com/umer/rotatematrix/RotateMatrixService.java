27d169ad4df54c9281c2bafb4c7b907dad68ccb7
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2023-01-22 18:32:45 +0100

    Feature: Created an empty class for rotating matrix service.

---
+++ src/com/umer/rotatematrix/RotateMatrixService.java
@@ -0,0 +1,19 @@
+package com.umer.rotatematrix;
+
+public class RotateMatrixService {
+	
+	/**
+	 * Given an image represented by an NxN matrix write a method to rotate the image by 90 degrees.
+	 * [1,2,3]		[7,4,1]
+	 * [4,5,6] 	=>	[8,5,2] 
+	 * [7,8,9]		[9,6,3]
+	 * 
+	 * @param matrix
+	 * @return
+	 */
+	  public boolean rotateMatrix(int[][] matrix) {
+	        // TODO
+		  return false;
+	    }
+
+}
