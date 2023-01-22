43067b5f07e8c072c43266d1725aeebcd7c22cb7
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2023-01-22 14:35:59 +0100

    Feature: Code reformatting.

--- src/com/umer/permutation/PermutationService.java
+++ src/com/umer/permutation/PermutationService.java
@@ -14,33 +14,33 @@ public class PermutationService {
 	 * 
 	 * true
 	 * 
-	 * @param array1
-	 * @param array2
+	 * @param firstArray
+	 * @param secondArray
 	 * @return
 	 */
-	public boolean permutation(int[] array1, int[] array2) {
+	public boolean permutation(int[] firstArray, int[] secondArray) {
 		// Check if the length is same or not. If there are not of equal length, then
 		// the arrays are not permutations of each other.
-		if (array1.length != array2.length) {
+		if (firstArray.length != secondArray.length) {
 			return false;
 		}
 		// Here, we check if the sum and the product of the arrays are same or not.
 		// If both of these values are equal, then the given arrays are permuations of
 		// each other.
 
-		int sum1 = 0;
-		int sum2 = 0;
-		int product1 = 1;
-		int product2 = 1;
+		int sumOfFirstArray = 0;
+		int sumOfSecondArray = 0;
+		int productOfFirstArray = 1;
+		int productOfSecondArray = 1;
 
-		for (int i = 0; i < array2.length; i++) {
-			sum1 += array1[i];
-			sum2 += array2[i];
+		for (int index = 0; index < secondArray.length; index++) {
+			sumOfFirstArray += firstArray[index];
+			sumOfSecondArray += secondArray[index];
 
-			product1 *= array1[i];
-			product2 *= array2[i];
+			productOfFirstArray *= firstArray[index];
+			productOfSecondArray *= secondArray[index];
 		}
-		if (sum1 == sum2 && product1 == product2) {
+		if (sumOfFirstArray == sumOfSecondArray && productOfFirstArray == productOfSecondArray) {
 			return true;
 		} else {
 			return false;
