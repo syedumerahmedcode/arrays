3d178367795a7433e1d8e37ef989ff1d25c2fbc9
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2023-01-24 19:38:17 +0100

    Feature: Created an empty class for finding the best first and second score in an array.

---
+++ src/com/umer/bestscore/BestScoreService.java
@@ -0,0 +1,24 @@
+package com.umer.bestscore;
+
+public class BestScoreService {
+
+	/**
+	 * Best Score
+	 * 
+	 * Given an array, write a function to get first, second best scores from the
+	 * array.
+	 * 
+	 * Array may contain duplicates.
+	 * 
+	 * Example
+	 * 
+	 * myArray = {84,85,86,87,85,90,85,83,23,45,84,1,2,0} 
+	 * firstSecond(myArray) // 90 87
+	 * 
+	 * @param myArray
+	 * @return
+	 */
+	static String firstSecond(Integer[] myArray) {
+		return null;
+	}
+}
