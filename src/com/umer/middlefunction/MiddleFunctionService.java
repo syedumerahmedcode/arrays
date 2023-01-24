d4746c39c8b023d25e945ae31a84f0f8299a19fc
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2023-01-24 19:06:21 +0100

    Feature: Implemented the logic for finding the array which does not contain first and last elements of the input array.

--- src/com/umer/middlefunction/MiddleFunctionService.java
+++ src/com/umer/middlefunction/MiddleFunctionService.java
@@ -1,19 +1,20 @@
 package com.umer.middlefunction;
 
+import java.util.Arrays;
+
 public class MiddleFunctionService {
 
 	/**
 	 * Write a function called middle that takes a list and returns a new list that
 	 * contains all but the first and last elements.
 	 * 
-	 * myArray = [1, 2, 3, 4] 
-	 * middle(myArray) # [2,3]
+	 * myArray = [1, 2, 3, 4] middle(myArray) # [2,3]
 	 * 
 	 * @param arr
 	 * @return
 	 */
 	public int[] middle(int[] arr) {
-		// TODO
+		return Arrays.copyOfRange(arr, 1, arr.length - 1);
 	}
 
 }
