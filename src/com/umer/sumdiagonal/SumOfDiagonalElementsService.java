b8acdd170ac625a557ca41c07197e897d3157ce8
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2023-01-24 19:11:00 +0100

    Feature: Created an empty class for finding sum of diagonal elemnts in a 2D array.

---
+++ src/com/umer/sumdiagonal/SumOfDiagonalElementsService.java
@@ -0,0 +1,22 @@
+package com.umer.sumdiagonal;
+
+public class SumOfDiagonalElementsService {
+
+	/**
+	 * Given 2D array calculate the sum of diagonal elements.
+	 * 
+	 * Example
+	 * 
+	 * myArray2D= {{1,2,3},{4,5,6},{7,8,9}};   
+	 * 
+	 * sumDiagonal(myArray2D) # 15
+	 * 
+	 * @param a
+	 * @return
+	 */
+	public int sumDiagonal(int[][] a) {
+		return -1;
+
+	}
+
+}
