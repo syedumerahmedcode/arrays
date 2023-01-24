01d5b08b4859590a71a7613d61daa364086c0a2d
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2023-01-24 18:52:50 +0100

    Feature: Created an empty class for finding the middle part of an array.

---
+++ src/com/umer/middlefunction/MiddleFunctionService.java
@@ -0,0 +1,19 @@
+package com.umer.middlefunction;
+
+public class MiddleFunctionService {
+
+	/**
+	 * Write a function called middle that takes a list and returns a new list that
+	 * contains all but the first and last elements.
+	 * 
+	 * myArray = [1, 2, 3, 4] 
+	 * middle(myArray) # [2,3]
+	 * 
+	 * @param arr
+	 * @return
+	 */
+	public int[] middle(int[] arr) {
+		// TODO
+	}
+
+}
