078f41eb067acc49e6278a24380bfefc29a4ca34
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2023-01-25 19:41:39 +0100

    Feature: Added comments in the logic of the function for better understanding.

--- src/com/umer/bestscore/BestScoreService.java
+++ src/com/umer/bestscore/BestScoreService.java
@@ -22,29 +22,41 @@ public class BestScoreService {
 	 * @return
 	 */
 	public String firstSecond(Integer[] inputArray) {
-		// If we were to only solve it using arrays, then we need to loop through the entire array once and store highest and second highest numbers in two variables.
-		// However, since we do not have any restrictions on using other classes, we are using Collections class.
+		// If we were to only solve it using arrays, then we need to loop through the
+		// entires of the array once and store highest and second highest numbers in two
+		// variables.
 		
+		// However, since we do not have any restrictions on using other classes, we are
+		// using Collections class.
+
 		// First we create a duplicate of the input array.
-		Integer[] arr = inputArray;
-		
+		Integer[] tempArray = inputArray;
+
 		// Next, we sort the array in descending order.
-		Arrays.sort(arr, Collections.reverseOrder());
-		
-		// Now, because the array is sorted in descending order, the first element in the array is the biggest number. 
-		int first = arr[0];
-		
-		// Second number is initialized to null. Why? Because the input array can contain duplicates so we cannot just blindly take the value at index 1.
-		Integer second = null;
-		
-		// We iterate through the length of the array and look out for first number which is different from the first number.
-		// Since, the array is sorted in descending order ... to be continued.
-		for (int index = 0; index < arr.length; index++) {
-			if (arr[index] != first) {
-				second = arr[index];
+		Arrays.sort(tempArray, Collections.reverseOrder());
+
+		// Now, because the array is sorted in descending order, the first element in
+		// the array is the biggest number.
+		int firstNumber = tempArray[0];
+
+		// Second number is initialized to null. Why? Because the input array can
+		// contain duplicates so we cannot just blindly take the value at index 1.
+		Integer secondNumber = null;
+
+		// We iterate through the length of the array and look out for first number
+		// which is different from the first number.		
+		// Since, the array is sorted in descending order, we should look for the first
+		// number which is different from first number.		
+		// Why? So that we can avoid duplicates.
+		for (int index = 0; index < tempArray.length; index++) {
+			if (tempArray[index] != firstNumber) {
+				secondNumber = tempArray[index];
+				// Now that we have found the second number, there is no need to loop through
+				// any further and hence, we break out of the loop.
 				break;
 			}
 		}
-		return first + " " + second;
+		// Finally, the first and second highest number is returned as a string.
+		return firstNumber + " " + secondNumber;
 	}
 }
