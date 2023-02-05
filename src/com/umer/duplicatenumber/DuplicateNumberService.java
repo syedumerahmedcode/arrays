6cc3a9abca201661dac17bd405a0222e63114db7
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2023-02-05 15:55:16 +0100

    Feature: Created an empty class for finding duplicate number service.

---
+++ src/com/umer/duplicatenumber/DuplicateNumberService.java
@@ -0,0 +1,19 @@
+package com.umer.duplicatenumber;
+
+public class DuplicateNumberService {
+
+	/**
+	 * Write a function to find the duplicate number on given integer array.
+	 * 
+	 * Example
+	 * 
+	 * removeDuplicates({1, 1, 2, 2, 3, 4, 5}) 
+	 * Output : [1, 2, 3, 4, 5]
+	 * 
+	 * @param arr
+	 * @return
+	 */
+	public static int[] removeDuplicates(int[] arr) {
+		
+	}
+}
