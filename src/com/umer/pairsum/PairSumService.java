e0886a26757bb801830fe741e721d1725f8cff57
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2023-02-07 17:01:57 +0100

    Feature: Created an empty class for finding pairs whose sum is equal to the given number.

---
+++ src/com/umer/pairsum/PairSumService.java
@@ -0,0 +1,20 @@
+package com.umer.pairsum;
+
+public class PairSumService {
+	/**
+	 * Write a function to find all pairs of an integer array whose sum is equal to
+	 * a given number.
+	 * 
+	 * Example
+	 * 
+	 * pairSum({2, 4, 3, 5, 6, -2, 4, 7, 8, 9},7) Output : "2:5 4:3 3:4 -2:9 "
+	 * 
+	 * @param myArray
+	 * @param sum
+	 * @return
+	 */
+	public String pairSum(int[] myArray, int sum) {
+		return null;
+	}
+
+}
